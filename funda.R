815dbfedf621d8a9ca887fc91408c3d01dae3291
gpoudel <gaurab.poudel@gmail.com>
2017-07-31 00:05:41 +0200

    Added functions whcih return the full DF, number of houses in a city and number of pages of web data

--- funda.R
+++ funda.R
@@ -8,19 +8,57 @@ gc()
 library("tidyverse")
 library("RJSONIO")
 library("DT")
+library("data.table")
 
 
-funda_ranking_utrecht <- "https://partnerapi.funda.nl/feeds/Aanbod.svc/json/271175433a7c4fe2a45750d385dd9bfd/?type=koop&zo=/amsterdam/&page=1&pagesize=25"
+results_per_page <- 25  #This (25) is the max possible and hard coaded
+
+
+#function find toal number of houses in city and number of pages of web data 
+total.houses <- function(location) {
+  
+  rankings.link <- paste0('https://partnerapi.funda.nl/feeds/Aanbod.svc/json/271175433a7c4fe2a45750d385dd9bfd/?type=koop&zo=/',location,'/&page=1&pagesize=25')
+  
+  ranking.db <- fromJSON(rankings.link)
+  
+  house.count <- c( "total.houses" = ranking.db$TotaalAantalObjecten, "total.pages" = ranking.db$Paging$AantalPaginas)
+  
+  return(house.count)
+  
+
+}
 
 
 
-utrecht <- fromJSON(funda_ranking_utrecht)
+#function that returns a matrix 
+city.db <- function(location,total.houses,total.pages) {
+  
+  city.df <- c()
+  
+  for (i in 1:total.pages)
+  {
+    city.link <- paste0('https://partnerapi.funda.nl/feeds/Aanbod.svc/json/271175433a7c4fe2a45750d385dd9bfd/?type=koop&zo=/',location,'/&page=',i,'&pagesize=25')    
+    
+    city <- fromJSON(city.link)
+    
+     for (j in 1:25)
+     {
+       try(city.df <- rbind(city.df,t(city$Objects[[j]])))
+       
+     }
+    
+  }
+  
+  return(city.df)
+  
+}
 
-utrecht_df <- c()
 
 
- for (i in 1:25) {
-   
-   utrecht_df <- rbind(utrecht_df,t(utrecht$Objects[[i]]))
-   
- }
+utrecht.info <- total.houses('utrecht')
+
+utrecht.df <- city.db('utrecht', utrecht.info[1], utrecht.info[2])
+
+
+
+
